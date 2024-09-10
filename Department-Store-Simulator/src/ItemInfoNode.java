13ff6554571c2e6e0615ccc0dea0880b1c150ace
Ian Kaufman <Kaufmanian49@gmail.com>
2024-09-09 20:14:50 -0400

    Update ItemInfoNode.java

--- Department-Store-Simulator/src/ItemInfoNode.java
+++ Department-Store-Simulator/src/ItemInfoNode.java
@@ -1,10 +1,4 @@
-/*
- * Ian Kaufman
- * 115639955
- * ian.kaufman@stonybrook.edu
- * Homework 2
- * CSE214.R30
- */
+
 /**
  * The ItemInfoNode class represents a node in the double-linked list of items.
  */
